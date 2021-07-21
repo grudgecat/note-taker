c3f2cff29d9273df81f066694497fe86a435b090
grudgecat <grudgecat@protonmail.com>
2021-07-20 21:35:25 -0700

    adjust file structure to add public folder

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
+
+//set up routes
+app.get('/', (req, res) => 
+
+app.get('/', (req, res) => 
+
+app.get('/', (req, res) => 
+
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
